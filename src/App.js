59264be750cd244434f2e8f9c29d948d699ca8da
car <carmelogustavo@gmail.com>
2019-01-09 22:21:01 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './acasa.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>MascotApp</h1>
+        <h2>En construcción</h2>
+        <img src={logo} alt="logo" />
       </div>
     );
   }
